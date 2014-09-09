0e6ad6b3bcb65e37237378ebe37bef47725312bd
Ijeoma <gift_ij2002@yahoo.com>
2014-09-09 16:35:49 +0100

    updated

--- scripts/jsfiles.js
+++ scripts/jsfiles.js
@@ -1,19 +1,39 @@
-$(document).ready(function() {
-	$('div#alert').hide();
-	$('input#add').click(function(event) {
+var shopper = {
+ 
+	addToList: function(event) {
 		event.preventDefault();
-		var itemAdd = document.getElementById('intro').value;
-			if (itemAdd === "undefined" || itemAdd === " " || itemAdd.length < 3 || !isNaN(itemAdd)) {
-				var response = '<p>Please type in a valid input</p>';
-				$('div#alert').fadeIn('slow').append(response).fadeOut(1000);
-		}
-			else {
-				$('ul.check').append('<input type= "checkbox" name= "checkbox" class= "checkbox">' + itemAdd  + "<br>")
-				$('input#intro').val('');
+		itemAdd = $('#intro').val();
+		if (itemAdd === "undefined" || itemAdd === " " || itemAdd.length < 3 || !isNaN(itemAdd)) {
+			$('div#alert').fadeIn('slow').text('Please type in a valid input').fadeOut(1000);
+			console.log("processed");
+		} 
+		else {
+			$('ul.check').append('<li><input type= "checkbox" name= "checkbox" class= "checkbox">'+ itemAdd + '</li>')
+			$('input#intro').val('');
+		};
+	},
+
+	deleteFromList: function() {
+		event.preventDefault();
+		var reValidate = false;
+		var check = $('input[type= "checkbox"]');
+		$(check).each(function() {
+			if (this.checked) {
+				$(this).parent().remove();
+				reValidate = true;
 			}
- 	});
-//  	$('input#delete').click(function(event) {
-//  		event.preventDefault();
-//  			$('ul.check:checked' + itemAdd).remove();
-// });
- });
+		});
+		if (!reValidate) {
+			$('div#alert').fadeIn('slow').text('No item to delete').fadeOut(1000);
+		}
+	},		
+
+	init: function() {
+		$('#alert').hide();
+		var itemAdd;
+		$('#add').click(shopper.addToList);
+		$('#delete').click(shopper.deleteFromList);
+	}
+}
+
+$(document).ready(shopper.init);
